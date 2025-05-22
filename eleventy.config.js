7e311ae59815e36bfb0b27faa919e564b3bd63b5
uncouthregions <40957679+uncouthregions@users.noreply.github.com>
2025-05-22 17:20:06 +0100

    Phosphor

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,16 +7,21 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import pluginFilters from "./_config/filters.js";
 //import footnote_plugin from 'markdown-it-footnote';
 
-
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function(eleventyConfig) {
 //	eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(footnote_plugin));
 	// Drafts, see also _data/eleventyDataSchema.js
+	
 	eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
 		if(data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
 			return false;
 		}
 	});
+	
+	const eleventyPluginPhosphoricons = require('eleventy-plugin-phosphoricons');
+	module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyPluginPhosphoricons);
+};
 
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
