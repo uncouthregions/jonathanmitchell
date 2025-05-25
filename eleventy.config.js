72d897fc780321cc29318a64b75b1a9bdbd7828f
uncouthregions <40957679+uncouthregions@users.noreply.github.com>
2025-05-25 13:50:11 +0100

    Add markdown-it-footnote

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,8 +5,7 @@ import pluginNavigation from "@11ty/eleventy-navigation";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 import pluginFilters from "./_config/filters.js";
-//import footnote_plugin from 'markdown-it-footnote';
-
+import footnote_plugin from 'markdown-it-footnote';
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default async function(eleventyConfig) {
