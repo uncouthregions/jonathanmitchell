9d9e2457edd88635f226479bd6503d3b2e5be443
uncouthregions <40957679+uncouthregions@users.noreply.github.com>
2025-05-25 22:42:44 +0100

    url formatting

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,10 +2,10 @@ export default {
 	title: "Jonathan Paul Mitchell",
 	url: "https://jonathanmitchell.me/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Personal website and blog.",
 	author: {
 		name: "Jonathan Paul Mitchell",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		email: "hello@jonathanmitchell.me",
+		url: "https://jonathanmitchell.me/about/"
 	}
 }
