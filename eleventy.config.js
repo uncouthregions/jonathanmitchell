4bb65d018470098e81afa90047d4b37e1f81a26e
uncouthregions <40957679+uncouthregions@users.noreply.github.com>
2025-05-25 23:08:06 +0100

    rss

--- eleventy.config.js
+++ eleventy.config.js
@@ -22,7 +22,7 @@ export default async function(eleventyConfig) {
 		.addPassthroughCopy({
 			"./public/": "/"
 		})
-		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
+		.addPassthroughCopy("./content/pretty-atom-feed.xsl");
 
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
